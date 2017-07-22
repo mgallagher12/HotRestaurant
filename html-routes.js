7a53136238fb5e9ab0cf0d07077af9cb59abe224
Karen Manley <Karen Manley>
2017-07-22 10:23:14 -0400

    html-routes.js

---
+++ html-routes.js
@@ -0,0 +1,12 @@
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/reservations", function(req, res) {
+  res.sendFile(path.join(__dirname, "reservations.html"));
+});
+
+app.get("/tables", function(req, res) {
+  res.sendFile(path.join(__dirname, "tables.html"));
+});

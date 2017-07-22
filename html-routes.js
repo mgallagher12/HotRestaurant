eedbfe680d69148c8d1b2796005cda266c0fe6c9
Matthew Gallagher <matthewgallagher@Matthews-MacBook-Pro.local>
2017-07-22 11:05:01 -0400

    Pushed Gitignore

---
+++ html-routes.js
@@ -0,0 +1,16 @@
+module.exports = function (app){
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/reservations", function(req, res) {
+  res.sendFile(path.join(__dirname, "reservations.html"));
+});
+
+app.get("/tables", function(req, res) {
+  res.sendFile(path.join(__dirname, "tables.html"));
+});
+
+}
